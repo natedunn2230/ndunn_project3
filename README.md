891586b1724da03090d05286baf86dc84fd9621f
natedunn2230 <dunn.113@wright.edu>
2019-11-10 16:06:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project 3: Work Efficient Parallel Reduction / Work Efficient Parallel Prefix Sum
+#### CS-4370-90
+#### Nathan Dunn
+#### Professor Liu
+#### 11-18-19
---
+++ ndunn_project3.c
@@ -0,0 +1,19 @@
+/**
+ * Nathan Dunn
+ * Project 3: Work Efficient Parallel Reduction and Work Efficient Parallel Prefix Sum
+ * Professor Liu
+ * CS-4370-90
+ * 11-18-19
+*/
+
+#include <stdio.h>
+#include <cuda.h>
+
+
+int main(void){
+	
+	printf("Hello world!\n");
+	
+	return 0;
+	
+}
